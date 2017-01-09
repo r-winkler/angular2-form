dc84045b25ec3a7c55c2c59b04c6460b43fd927b
René Winkler <rene.winkler@xovis.com>
2017-01-09 19:41:44 +0100

    validation

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,12 +8,22 @@ import {Employee} from './employee.model';
 })
 export class EmployeeComponent {
     languages = ['German', 'English', 'Spanish', 'Other'];
-    model = new Employee('René', 'Winkler', true, 'w2', 'German');
+    model = new Employee('René', 'Winkler', true, 'w2', 'default');
+    
     
     lastNameToUpperCase(value: string) {
         if(value.length > 0) 
             this.model.lastName = value.charAt(0).toUpperCase() + value.slice(1);
         else
             this.model.lastName = value;
+    }
+    
+    hasPrimaryLanguageError = false;
+    validatePrimaryLanguage(value) {
+        if (value === 'default') {
+            this.hasPrimaryLanguageError = true;
+        }
+        else
+            this.hasPrimaryLanguageError = false;
     }
 }
