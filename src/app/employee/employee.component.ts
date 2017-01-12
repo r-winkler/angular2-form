3826488ee5a235d549a6813b91a09a79dbc07211
René Winkler <rene.winkler@xovis.com>
2017-01-12 20:52:08 +0100

    implement http service

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -9,7 +9,7 @@ import {NgForm} from '@angular/forms';
   templateUrl: './employee.component.html'
 })
 export class EmployeeComponent {
-    languages = ['German', 'English', 'Spanish', 'Other'];
+    languages = [];
     model = new Employee('René', 'Winkler', true, 'w2', 'default');
     
     
@@ -23,6 +23,11 @@ export class EmployeeComponent {
     hasPrimaryLanguageError = false;
     
     constructor(private formPoster: FormPoster) {
+        this.formPoster.getLanguages()
+            .subscribe(
+                data => this.languages = data.languages,
+                err => console.log('get error: ', err)
+            );
     
     }
     
@@ -32,7 +37,11 @@ export class EmployeeComponent {
         this.validatePrimaryLanguage(this.model.primaryLanguage);
         if (this.hasPrimaryLanguageError)
             return;
-        this.formPoster.postEmployeeForm(this.model);
+        this.formPoster.postEmployeeForm(this.model)
+            .subscribe(
+                data => console.log('success:', data),
+                err => console.log('error:', err)
+            );
     }
     
     validatePrimaryLanguage(value) {
--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import {Employee} from './employee.model';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/Rx'
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class FormPoster {
@@ -9,7 +11,29 @@ export class FormPoster {
     
     }
     
-    postEmployeeForm(employee: Employee) {
-        console.log('posting form', employee);
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.fields || {};
+    }
+    
+    private extractLanguages(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+    
+    private handleError(error: any) {
+        console.log('post error: ', error);
+        return Observable.throw(error.statusText);
+    }
+    
+    getLanguages(): Observable<any> {
+        return this.http.get('http://localhost:3100/get-languages').map(this.extractLanguages).catch(this.handleError);
+    }
+    
+    postEmployeeForm(employee: Employee):Observable<any> {
+        let body = JSON.stringify(employee);
+        let headers = new Headers({'Content-Type':'application/json'});
+        let options = new RequestOptions({headers:headers});
+        return this.http.post('http://localhost:3100/postemployee', body, options).map(this.extractData).catch(this.handleError);
     }
 }
